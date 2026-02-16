f3f7c8bad2506cf34abc5b62a85316ce49a93519
Wyatt Yousey <Quiz.nv1@gmail.com>
2026-02-15 18:56:23 -0600

    Time: 4 ms (87.52%) | Memory: 57.2 MB (80.62%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const set = new Set();
+    let left = 0;
+    let max = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while(set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        max = Math.max(max, right - left + 1)
+    }
+    return max;
+};
